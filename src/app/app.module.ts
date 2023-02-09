cf3e228eb74459c3b8fe310ef6e71e529f60e1ae
Sahil <ajmerisahli94@gmail.com>
2023-02-09 10:29:43 +0530

    For loop and if-else statement code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//import { NavbarModule } from './navbar/navbar.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+//import { MyCardComponent } from './my-card/my-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+ //   MyCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,//NavbarModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
