a288996b6cea07527c8bd9ba8a822572d2f9a3fe
Sahil <ajmerisahli94@gmail.com>
2023-02-16 14:15:02 +0530

    new addition of files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http'
-import {UserDataService} from './users/user-data.service'
+//import {UserDataService} from './users/user-data.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- user:any;
- getApi(){
-  this. userData.users().subscribe((data)=>this.user=data);
-
- }
- 
-  constructor( private userData:UserDataService){
-      this.getApi();
-          
-  }
-
+ f1:boolean=false;
+ f2:boolean=false;
+f3=true;
+name="sahill";
+//green="green"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
-
+import { NgIf } from '@angular/common';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
+import { Router, RouterModule, ROUTES, Routes } from '@angular/router';
 //import { FormComponent } from './form/form.component';
 
+const myrouter:Routes=[{
+path:' ',
+component:AppComponent
+},
+{
+  path:'parent',
+  component:ParentComponent
+},
+{
+  path:'child/:id/:name',
+  component:ChildComponent
+}
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    ParentComponent,
+    ChildComponent,
+   
    
   
    // FormComponent,
@@ -21,7 +42,10 @@ import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgIf,
+    RouterModule.forRoot(myrouter)
+    
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
